6c262b6566e9d98c8c64a409f27e8684ddd93ba2
Edson Martins <edsondizeseis@hotmail.com>
2024-07-15 02:48:37 -0300

    a

--- app/scripts/scale_app.sh
+++ app/scripts/scale_app.sh
@@ -1,11 +1,36 @@
 #!/bin/bash
 
+# Variáveis
 APP_NAME="marrige-back"
+HEROKU_API_KEY="HRKU-0ebc5eb7-5b9c-4a65-b4b8-0b0e70efe112"
 
+# Verifique o argumento de entrada
 if [ "$1" == "start" ]; then
-    heroku ps:scale web=1 --app $APP_NAME
+    # Comando para ligar a aplicação
+    curl -n -X PATCH https://api.heroku.com/apps/$APP_NAME/formation \
+    -H "Authorization: Bearer $HEROKU_API_KEY" \
+    -H "Accept: application/vnd.heroku+json; version=3" \
+    -d '{
+      "updates": [
+        {
+          "type": "web",
+          "quantity": 1
+        }
+      ]
+    }'
 elif [ "$1" == "stop" ]; then
-    heroku ps:scale web=0 --app $APP_NAME
+    # Comando para desligar a aplicação
+    curl -n -X PATCH https://api.heroku.com/apps/$APP_NAME/formation \
+    -H "Authorization: Bearer $HEROKU_API_KEY" \
+    -H "Accept: application/vnd.heroku+json; version=3" \
+    -d '{
+      "updates": [
+        {
+          "type": "web",
+          "quantity": 0
+        }
+      ]
+    }'
 else
     echo "Uso: $0 {start|stop}"
     exit 1
